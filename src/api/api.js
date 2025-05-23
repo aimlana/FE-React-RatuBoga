0b0dee035932c2449c1e877e263393420bf2d372
Muh. Salim Maulana <salim.yaklu@gmail.com>
2025-05-23 21:23:53 +0800

    feat: :art: add api key

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'http://localhost:5001/api',
-  headers: { 'Content-Type': 'application/json' },
+  headers: {
+    'Content-Type': 'application/json',
+    'x-api-key': '513MA-77EI7I8A-101278RB',
+  },
 });
 
 export default API;
