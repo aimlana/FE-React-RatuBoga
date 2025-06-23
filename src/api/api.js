df1bce5b391aa1648898f701b50d2ae69dfbdb28
Muh. Salim Maulana <salim.yaklu@gmail.com>
2025-06-23 21:31:39 +0800

    chore(env): :lock: move API key to .env file for security

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 const API = axios.create({
   baseURL: 'http://localhost:5001/api',
   headers: {
     'Content-Type': 'application/json',
-    'x-api-key': '513MA-77EI7I8A-101278RB',
+    'x-api-key': API_KEY,
   },
+});
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default API;
